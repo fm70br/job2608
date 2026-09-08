32612b9f8ca24dbba1e9bbc77776f6a6cfa709d3
Fabio Minami <fm70dev@gmail.com>
2026-09-08 20:37:02 -0300

    Primeiro commit

---
+++ trabalho.py
@@ -0,0 +1,70 @@
+from operator import truediv
+
+
+print("CONTROLE DE PRODUÇÃO E QUALIDADE\n")
+print("1. Cadastrar nova peça")
+print("2. Listar peças aprovadas/reprovadas")
+print("3. Remover peça cadastrada")
+print("4. Listar caixas fechadas")
+print("5. Gerar relatório final\n")
+
+opcao = int(input("escolha 1 a 5: "))
+
+match opcao:
+    case 1:
+        print("Informe (id, peso, cor e comprimento)")
+    case 2:
+        print("escolheu 2")
+    case 3:
+        print("escolheu 3")        
+    case 4:
+        print("4")
+    case 5:
+        print("5")
+    case _:
+        print("Opção inválida")
+
+## (id, peso, cor e comprimento)
+##  - Peso entre 95g e 105g
+##  - Cor azul ou verde
+##  - Comprimento entre 10cm e 20cm
+
+def ver_peso(peso):
+    if peso >= 95 and peso <= 105:
+        return True
+    else:
+        return False
+
+def ver_cor(cor):
+    if cor == "azul" or cor == "verde":
+        return True
+    else:
+        return False
+
+def ver_comprimento(comprimento):
+    if comprimento >= 10 and comprimento <= 20:
+        return True
+    else:
+        return False
+
+
+print(ver_peso(100))
+print(ver_cor("verde"))
+print(ver_comprimento(15))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
